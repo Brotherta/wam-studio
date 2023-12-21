353b143bdf6ade2eedbc8767e8e86def682470e6
micbuffa <micbuffa@gmail.com>
2023-12-21 08:37:57 +0100

    fixed cors

--- bank/src/index.js
+++ bank/src/index.js
@@ -15,13 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(config.corsOptions));
 app.use(cookieParser());
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
 
-    next();
-});
 
 utils.checkEnvVars();
 utils.createDirectories();
