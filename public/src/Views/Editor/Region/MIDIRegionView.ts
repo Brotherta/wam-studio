d7c1fc9b2585982e82484ffe12c3d1ab06a3caf4
Jempasam <samueldemont83@gmail.com>
2024-06-25 16:05:34 +0200

    MIDI Region View better suited for MIDI Note format

--- public/src/Views/Editor/Region/MIDIRegionView.ts
+++ public/src/Views/Editor/Region/MIDIRegionView.ts
@@ -33,18 +33,13 @@ export default class MIDIRegionView extends RegionView<MIDIRegion> {
         target.beginFill(colorHex, 0.5);
 
         // Get max amplitude
-        let minnote=Infinity
-        let maxnote=-Infinity
+        let minnote=50
+        let maxnote=78
         region.midi.forEachNote((note, start)=>{
             if(note.note<minnote) minnote=note.note
             if(note.note>maxnote) maxnote=note.note
         })
         let amplitude=maxnote-minnote
-        if(amplitude<100){
-            minnote-=(100-amplitude)/2
-            maxnote+=(100-amplitude)/2
-            amplitude=100
-        }
 
         // Draw notes
         const note_height=(HEIGHT_TRACK-HEIGHT_TRACK/10)/amplitude
@@ -54,7 +49,7 @@ export default class MIDIRegionView extends RegionView<MIDIRegion> {
             const y=local_note*note_height
             const x=start*note_width
             const w=Math.max(1,note.duration*note_width)
-            const h=HEIGHT_TRACK/10
+            const h=HEIGHT_TRACK/128*5
             target.drawRect(x, y, w, h)
             target.drawRect(x+w-HEIGHT_TRACK/20, y, HEIGHT_TRACK/20, h)
         })
